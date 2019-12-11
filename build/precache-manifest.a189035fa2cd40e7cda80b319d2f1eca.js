6ab29d52c0c3c4f6f44d979de737bb4fcd1fd3de
Ilya <ilyaphotoshop17@gmail.com>
2019-12-11 23:56:32 +0200

    build ready

---
+++ build/precache-manifest.a189035fa2cd40e7cda80b319d2f1eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868d4db1d01aeef26853d28f9d8442b1",
+    "url": "/Tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9d3c6695944362cfcd35",
+    "url": "/Tic-tac-toe/static/css/main.bcf37ae7.chunk.css"
+  },
+  {
+    "revision": "dca9a01ac5bcee12b75e",
+    "url": "/Tic-tac-toe/static/js/2.fb217499.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tic-tac-toe/static/js/2.fb217499.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d3c6695944362cfcd35",
+    "url": "/Tic-tac-toe/static/js/main.dbbf4720.chunk.js"
+  },
+  {
+    "revision": "ef81bd6a91a4b2161c1b",
+    "url": "/Tic-tac-toe/static/js/runtime-main.550d4707.js"
+  }
+]);
